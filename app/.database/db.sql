f9fb934bde4d07db5fa2a7220e05f64d9f1619a2
Ricardo Goncalves <ricardo@vitrynedigital.com.br>
2023-01-02 16:25:13 -0300

    first commit

--- app/.database/db.sql
+++ app/.database/db.sql
@@ -5,20 +5,6 @@ create table users
     email            varchar(100)  not null,
     name             varchar(100)  not null,
     password         varchar(100)  null,
-    drinkcounter     int default 0 null,
     token            varchar(150)  null,
     token_expiration datetime      null
-);
-
-create table user_drinks
-(
-    id             int auto_increment,
-    user_iduser    int           not null,
-    date_reference date          not null,
-    count          int default 0 not null,
-    primary key (id, user_iduser),
-    constraint unique_key
-        unique (date_reference, user_iduser),
-    constraint user_drinks_users_null_fk
-        foreign key (user_iduser) references users (iduser)
 );
